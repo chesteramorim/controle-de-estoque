0d955f1ad117da1434b4b944440b4df673e36056
antoniolight213-wq <antoniolight213@gmail.com>
2026-03-02 17:45:02 -0300

    atualizado

--- src/banco.php
+++ src/banco.php
@@ -1,5 +1,6 @@
 <?php
-$usuario= "";
+$servidor = "localhost";
+$usuario= "root";
 $senha ="";
 $banco="estoque";
 
@@ -8,23 +9,24 @@ $banco="estoque";
 
 
 try{
+
+
+
 //tentativa de conexão com o banco de dados
 //código...
 //classe PDO é usada para acessar varios tipos de banco de dados usando a mesma função 
 //DSN Data source name é a string de conexão usada para especificar o tipo de banco de dados,o host e o nome do banco de dados  
 
+$conexao = new PDO ("mysql:host=$servidor;dbname=$banco;charset=utf8",$usuario, $senha);
 
-$conexao= new PDO("
-mysql:hos=$servidor;
-dbname=$banco;
-charset=utf8",
-$usuario,
-$senha);
 
+$conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+//echo"Conexão com o banco de dados estabelecida com sucesso!";
 
 } catch (\Throwable $erro) {
 
-//lançavel serve para qualquer tipo de erro ou exceção
-//captura de erro caso a conexão falhe
+die("Erro ao conectar com o banco de dados:". $erro->getMessage());
+
 
 }
